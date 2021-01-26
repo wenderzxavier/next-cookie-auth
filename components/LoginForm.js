86bb7debc3a5f6a0b18fbc06b856f0d1b60f8c77
Wender Z. Xavier <wender.zacariasxavier@gmail.com>
2021-01-26 11:20:48 -0300

    fetch user profile upon login

---
+++ components/LoginForm.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { loginUser } from "../lib/auth";
+
+class LoginForm extends React.Component {
+  state = {
+    email: "Sincere@april.biz",
+    password: "hildegard.org",
+  };
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    const { email, password } = this.state;
+    event.preventDefault();
+    loginUser(email, password);
+  };
+
+  render() {
+    const { email, password } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            value={email}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={password}
+            onChange={this.handleChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default LoginForm;
